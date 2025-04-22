5e964395355915625add0d5a5e14cd1a99c9cf59
Duong01 <nguyenvanduong10a5hh4@gmail.com>
2025-04-22 22:20:28 +0700

    Upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const authRoutes = require('./routes/auth.routes');
+const authRoutes = require('./model/auth.routes');
 
 const app = express();
 app.use(cors());
