f672d50304faf8c6d4e4716a13396b2e52859787
Duong01 <nguyenvanduong10a5hh4@gmail.com>
2025-04-22 23:29:00 +0700

    Upload

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth.routes");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+  origin: '*',
+  methods: ['GET','POST'],
+  credentials: true
+}));
 app.use("/api/auth", authRoutes);
 
 mongoose
