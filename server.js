8b70c802d9518364eedfed1ffe10718452ec8a27
Duong01 <nguyenvanduong10a5hh4@gmail.com>
2025-04-25 21:05:47 +0700

    Upload

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: '*',
+  origin: 'https://web-phim-wine.vercel.app',
   credentials: true
 }));
 app.use("/api/auth", authRoutes);
