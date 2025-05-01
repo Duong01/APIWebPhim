ffa022aaa0ddf9e35b01c5f0a1edde6c191720e4
Nguyễn Văn Dương <86581230+Duong01@users.noreply.github.com>
2025-05-01 21:21:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     'http://localhost:8080',
-    'https://web-phim-wine.vercel.app'
+    'https://web-phim-one.vercel.app'
   ],
   credentials: true
 }));
